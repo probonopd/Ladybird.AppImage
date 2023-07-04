f3ac101b560f18bb96cd6fc082f4aa9b68dbbf96
probonopd <probonopd@users.noreply.github.com>
2023-07-04 22:38:55 +0200

    AppDir and AppImage

--- build.sh
+++ build.sh
@@ -2,12 +2,24 @@
 
 # https://github.com/SerenityOS/serenity/blob/master/Documentation/BuildInstructionsLadybird.md
 
+#
+# Install build dependencies
+#
+
+sudo apt-get -y install build-essential cmake libgl1-mesa-dev ninja-build qt6-base-dev qt6-tools-dev-tools qt6-multimedia-dev qt6-wayland clang-15 clang++-15
+
+#
+# Get Ladybird source
+#
+
 git clone --depth 1 https://github.com/SerenityOS/serenity # TODO: Only the path at /Ladybird
+
+#
+# Build Ladybird
+#
 
 cd serenity/Ladybird
 ls
-
-sudo apt-get -y install build-essential cmake libgl1-mesa-dev ninja-build qt6-base-dev qt6-tools-dev-tools qt6-multimedia-dev qt6-wayland clang-15 clang++-15
 
 # Export CC environment variable to force clang-15 with ccache
 export CC="ccache clang-15"
@@ -22,3 +34,41 @@ cmake --build Build/
 find Build/
 ldd Build/ladybird
 ls -lh Build/
+
+#
+# Populate AppDir
+#
+
+mkdir -p Ladybird.AppDir/usr/bin Ladybird.AppDir/usr/share/applications Ladybird.AppDir/usr/share/icons/hicolor/256x256/apps
+cp Build/ladybird Ladybird.AppDir/usr/bin/
+strip Ladybird.AppDir/usr/bin/ladybird
+wget -c -q https://ladybird.dev/ladybird.png && mv ladybird.png Ladybird.AppDir/usr/share/icons/hicolor/256x256/apps/
+
+cat > Ladybird.AppDir/usr/share/applications/ladybird.desktop <<\EOF
+[Desktop Entry]
+Version=1.0
+Name=Ladybird
+Comment=Ladybird is an ongoing project to build an independent web browser from scratch
+Exec=ladybird
+Icon=ladybird
+Type=Application
+Categories=Network;WebBrowser;
+MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;
+StartupNotify=true
+EOF
+cp Ladybird.AppDir/usr/share/applications/ladybird.desktop Ladybird.AppDir/
+
+#
+# Deploy all dependencies into AppDir
+#
+
+wget -c https://github.com/$(wget -q https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous -O - | grep "appimagetool-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)
+chmod +x appimagetool-*.AppImage
+
+./appimagetool-*.AppImage -s deploy Ladybird.AppDir/usr/share/applications/*.desktop # Bundle EVERYTHING
+
+#
+# Turn AppDir into AppImage
+#
+
+VERSION=$(git rev-parse --short HEAD) ./appimagetool-*.AppImage ./Ladybird.AppDir
