8eda89f025b35af1eb2fb47ff3ad96518c988e25
probonopd <probonopd@users.noreply.github.com>
2023-07-04 20:52:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-# git clone only the path at https://github.com/SerenityOS/serenity/tree/master/Ladybird with depth 1
-git clone --depth 1 --no-checkout https://github.com/SerenityOS/serenity.git serenity
-cd serenity
-git sparse-checkout init --cone
-git sparse-checkout set Ladybird
+git clone --depth 1 https://github.com/SerenityOS/serenity # TODO: Only the path at /Ladybird
 
-cd Ladybird
+cd serenity/Ladybird
 ls
 
 sudo apt-get -y install build-essential cmake libgl1-mesa-dev ninja-build qt6-base-dev qt6-tools-dev-tools qt6-multimedia-dev qt6-wayland
