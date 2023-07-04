f1b1c8405fecd77d44a334840e0efeef5818755d
probonopd <probonopd@users.noreply.github.com>
2023-07-04 21:15:27 +0200

    clang-15 clang++-15

--- build.sh
+++ build.sh
@@ -7,17 +7,11 @@ git clone --depth 1 https://github.com/SerenityOS/serenity # TODO: Only the path
 cd serenity/Ladybird
 ls
 
-sudo apt-get -y install build-essential cmake libgl1-mesa-dev ninja-build qt6-base-dev qt6-tools-dev-tools qt6-multimedia-dev qt6-wayland gcc-12
+sudo apt-get -y install build-essential cmake libgl1-mesa-dev ninja-build qt6-base-dev qt6-tools-dev-tools qt6-multimedia-dev qt6-wayland clang-15 clang++-15
 
-# Ladybird needs gcc-12, so make it the default
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100
-gcc --version
-
-# Export CC environment variable to force gcc-12
-export CC=$(which gcc-12)
-
-# The above is still not enough, so uninstall gcc-11
-sudo apt-get -y remove gcc-11
+# Export CC environment variable to force clang-15
+export CC=$(which clang-15)
+export CXX=$(which clang++-15)
 
 # Build in Build/
 mkdir -p Build/
@@ -25,3 +19,6 @@ cmake -GNinja -S . -B Build/
 # optionally, add -DCMAKE_CXX_COMPILER=<suitable compiler> -DCMAKE_C_COMPILER=<matching c compiler>
 cmake --build Build/
 ninja -C Build/ run
+
+find Build/
+wget -c -q "https://github.com/probonopd/go-appimage/suites/14058685897/artifacts/785575955" --trust-server-names
