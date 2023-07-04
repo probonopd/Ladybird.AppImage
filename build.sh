3f65882eb60b6f42af08cc6a8008e7da483ac6ea
probonopd <probonopd@users.noreply.github.com>
2023-07-04 21:01:40 +0200

    Ladybird needs gcc-12, so make it the default

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ ls
 
 sudo apt-get -y install build-essential cmake libgl1-mesa-dev ninja-build qt6-base-dev qt6-tools-dev-tools qt6-multimedia-dev qt6-wayland gcc-12
 
+# Ladybird needs gcc-12, so make it the default
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100
+
 # Build in Build/
 mkdir -p Build/
 cmake -GNinja -S . -B Build/
